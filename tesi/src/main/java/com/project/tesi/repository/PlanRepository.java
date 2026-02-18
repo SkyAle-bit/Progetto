477e547f9a9a9c20674b6ba4bf5c05da1c354f5e
Alessandro <81766448+SkyAle-bit@users.noreply.github.com>
2026-02-18 12:25:07 +0100

    Prima Versione

---
+++ tesi/src/main/java/com/project/tesi/repository/PlanRepository.java
@@ -0,0 +1,11 @@
+package com.project.tesi.repository;
+
+import com.project.tesi.model.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional; // <--- Assicurati di importare questo
+
+@Repository
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+    Optional<Plan> findByName(String name); // <--- AGGIUNGI QUESTA RIGA
+}
