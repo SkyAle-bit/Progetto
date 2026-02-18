565e266d6def7765574d640c3a3322fb429ade61
Tesi <alessandrostuccio@gmail.com>
2026-02-18 17:30:26 +0100

    Popolazione DB

--- tesi/src/main/java/com/project/tesi/repository/SubscriptionRepository.java
+++ tesi/src/main/java/com/project/tesi/repository/SubscriptionRepository.java
@@ -3,6 +3,8 @@ package com.project.tesi.repository;
 import com.project.tesi.model.Subscription;
 import com.project.tesi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
 import java.util.Optional;
 import java.util.List;
 
