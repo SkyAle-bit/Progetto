409dc0184c70f33329525e772128040c95f578df
Tesi <alessandrostuccio@gmail.com>
2026-04-21 16:29:30 +0200

    fix invio mail

--- tesi/src/main/java/com/project/tesi/TesiApplication.java
+++ tesi/src/main/java/com/project/tesi/TesiApplication.java
@@ -11,6 +11,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class TesiApplication {
 
     public static void main(String[] args) {
+        // Forza l'utilizzo di IPv4 per risolvere problemi di timeout (es. verso smtp.gmail.com su IPv6)
+        System.setProperty("java.net.preferIPv4Stack", "true");
         SpringApplication.run(TesiApplication.class, args);
     }
 }
